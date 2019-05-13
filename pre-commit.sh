56901b7911411f24842f9de86b2f666755043e14
ArinaSaara <50498674+ArinaSaara@users.noreply.github.com>
2019-05-13 10:29:21 +0300

    Update pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-if workbox generateSW workbox-config.js ; then
+if npm run r; then
+#if workbox generateSW workbox-config.js ; then
   git add sw.js
   exit 0
 else
   echo "Cannot generate sw.js"
   echo "Aborting"
 fi
+
